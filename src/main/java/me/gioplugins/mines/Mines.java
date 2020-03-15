42c57bc385a8df49e3f8dc990afde0686e8724bd
Gio4E20 <37852106+Gio4E20@users.noreply.github.com>
2020-03-15 15:47:39 -0300

    initial Commit

---
+++ src/main/java/me/gioplugins/mines/Mines.java
@@ -0,0 +1,12 @@
+package me.gioplugins.mines;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mines extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
